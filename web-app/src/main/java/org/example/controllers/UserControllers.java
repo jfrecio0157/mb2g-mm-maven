facab228d49bb20d314d10e5f2abe033c2a83543
jfrecio0157 <jfrecio@gmail.com>
2025-04-01 14:04:15 +0200

    Incorporacion del modulo web-app

---
+++ web-app/src/main/java/org/example/controllers/UserControllers.java
@@ -0,0 +1,11 @@
+package org.example.controllers;
+
+import org.example.entities.User;
+import org.example.UserCommand;
+
+public class UserControllers {
+    User saveUser (UserCommand command){
+        //Implementacion falsa
+        return new User();
+    }
+}
